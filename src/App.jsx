264a335a8cdfb0b071576e001f3c76e004e95fb5
Jeremy Cox <jeremy@theoutlet.org>
2023-08-17 14:27:12 -0400

    stub up amazon-clone home page and navbar design elements

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Header'
+import Home from './Home'
+
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Home />
+    </div>
+  )
+}
+
+export default App
---
+++ src/firebase.jsx
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDGwWaaRFYczTWrao0TgvSx5ovLMQdbapY',
+  authDomain: 'clone-74c29.firebaseapp.com',
+  projectId: 'clone-74c29',
+  storageBucket: 'clone-74c29.appspot.com',
+  messagingSenderId: '420104587379',
+  appId: '1:420104587379:web:1317999e67b8941b370206',
+  measurementId: 'G-DCLJDD0F3J'
+}
